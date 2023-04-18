7c0af146f299541c4c315b178ea7f45dded6ded7
NgHinjo <114802211+NgHinjo@users.noreply.github.com>
2023-04-18 09:31:11 +0800

    Add files via upload

---
+++ CS203/CS203/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace CS203
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateNode = new System.Windows.Forms.Button();
+            this.btnAdjacentNode = new System.Windows.Forms.Button();
+            this.txtedge1 = new System.Windows.Forms.TextBox();
+            this.txtedge2 = new System.Windows.Forms.TextBox();
+            this.edge1 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picGraph = new System.Windows.Forms.PictureBox();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.closeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).BeginInit();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCreateNode
+            // 
+            this.btnCreateNode.Location = new System.Drawing.Point(28, 35);
+            this.btnCreateNode.Name = "btnCreateNode";
+            this.btnCreateNode.Size = new System.Drawing.Size(176, 33);
+            this.btnCreateNode.TabIndex = 0;
+            this.btnCreateNode.Text = "Create Node";
+            this.btnCreateNode.UseVisualStyleBackColor = true;
+            this.btnCreateNode.Click += new System.EventHandler(this.btnCreateNode_Click);
+            // 
+            // btnAdjacentNode
+            // 
+            this.btnAdjacentNode.Location = new System.Drawing.Point(28, 83);
+            this.btnAdjacentNode.Name = "btnAdjacentNode";
+            this.btnAdjacentNode.Size = new System.Drawing.Size(176, 41);
+            this.btnAdjacentNode.TabIndex = 1;
+            this.btnAdjacentNode.Text = "Adjacent Nodes";
+            this.btnAdjacentNode.UseVisualStyleBackColor = true;
+            this.btnAdjacentNode.Click += new System.EventHandler(this.btnAdjacentNode_Click);
+            // 
+            // txtedge1
+            // 
+            this.txtedge1.Location = new System.Drawing.Point(242, 46);
+            this.txtedge1.Name = "txtedge1";
+            this.txtedge1.Size = new System.Drawing.Size(144, 22);
+            this.txtedge1.TabIndex = 2;
+            // 
+            // txtedge2
+            // 
+            this.txtedge2.Location = new System.Drawing.Point(242, 104);
+            this.txtedge2.Name = "txtedge2";
+            this.txtedge2.Size = new System.Drawing.Size(144, 22);
+            this.txtedge2.TabIndex = 3;
+            // 
+            // edge1
+            // 
+            this.edge1.AutoSize = true;
+            this.edge1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.edge1.Location = new System.Drawing.Point(275, 18);
+            this.edge1.Name = "edge1";
+            this.edge1.Size = new System.Drawing.Size(80, 25);
+            this.edge1.TabIndex = 4;
+            this.edge1.Text = "Edge 1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(275, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Edge 2";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnAdjacentNode);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnCreateNode);
+            this.groupBox1.Controls.Add(this.txtedge2);
+            this.groupBox1.Controls.Add(this.edge1);
+            this.groupBox1.Controls.Add(this.txtedge1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 37);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(417, 151);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Graph Details";
+            // 
+            // picGraph
+            // 
+            this.picGraph.Location = new System.Drawing.Point(12, 194);
+            this.picGraph.Name = "picGraph";
+            this.picGraph.Size = new System.Drawing.Size(417, 230);
+            this.picGraph.TabIndex = 7;
+            this.picGraph.TabStop = false;
+            this.picGraph.Click += new System.EventHandler(this.picGraph_Click);
+            this.picGraph.MouseClick += new System.Windows.Forms.MouseEventHandler(this.picGraph_MouseClick);
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip2.TabIndex = 9;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.closeToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openToolStripMenuItem.Text = "&Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveToolStripMenuItem.Text = "&Save";
+            // 
+            // closeToolStripMenuItem
+            // 
+            this.closeToolStripMenuItem.Name = "closeToolStripMenuItem";
+            this.closeToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.closeToolStripMenuItem.Text = "&Clear";
+            this.closeToolStripMenuItem.Click += new System.EventHandler(this.closeToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.picGraph);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picGraph)).EndInit();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateNode;
+        private System.Windows.Forms.Button btnAdjacentNode;
+        private System.Windows.Forms.TextBox txtedge1;
+        private System.Windows.Forms.TextBox txtedge2;
+        private System.Windows.Forms.Label edge1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picGraph;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem closeToolStripMenuItem;
+    }
+}
+
---
+++ CS203/CS203/Form1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace CS203
+{
+    public partial class Form1 : Form
+    {
+           //global variables
+        bool stopcreate;
+        int[,] matrix; //serve as the adjacent matrix
+        int countnodes; //serve as counter
+        String[] vertices; //nodes
+        Graphics graph;
+
+        public Form1()
+        {
+            InitializeComponent();
+            setclear();
+        }
+
+        //create method
+        public void setclear() 
+        {
+            // set the initialization of variable to clear
+            graph = picGraph.CreateGraphics();
+            stopcreate= false;
+            matrix = new int[50, 50];
+            vertices = new string[50];
+            countnodes = 0;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btnAdjacentNode_Click(object sender, EventArgs e)
+        {
+            if (stopcreate == true) 
+            {
+                //set or define the adjacent of two nodes
+            }
+        }
+
+        private void btnCreateNode_Click(object sender, EventArgs e)
+        {
+            stopcreate = false;
+        }
+
+        private void picGraph_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void picGraph_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left && stopcreate != true)
+            {
+                //when you click the mouse at the leftside
+                Rectangle rect = new Rectangle(e.X, e.Y, 35, 35);
+
+                //define the location of x and y coordinate
+                graph.FillEllipse(Brushes.Black, rect);
+
+                //the number and color of the nodes
+                countnodes++;
+
+                //the number will appear based on the nodes created
+                graph.DrawString(countnodes.ToString(), new Font("Arial", 12), Brushes.White, e.X + 12, e.Y + 12);
+
+            }
+            else 
+            {   
+                //right click the node will stop creating
+                stopcreate = true;
+            }
+        }
+
+        private void closeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            setclear();
+        }
+    }
+}
