e5fe42d23560f9e6232163f1ef42368cc67e8d2d
NgHinjo <114802211+NgHinjo@users.noreply.github.com>
2023-04-27 09:31:26 +0800

    Add files via upload

--- flat/flat/Form1.Designer.cs
+++ flat/flat/Form1.Designer.cs
@@ -285,6 +285,7 @@
             this.btnOpen.TabIndex = 4;
             this.btnOpen.Text = "Open";
             this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
             // 
             // btnSave
             // 
@@ -298,6 +299,7 @@
             this.btnSave.TabIndex = 3;
             this.btnSave.Text = "Save";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // panel3
             // 
--- flat/flat/Form1.cs
+++ flat/flat/Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
 
 
 namespace flat
@@ -56,7 +58,7 @@ namespace flat
         int countnodes; //serve as counter
         String[] vertices; //nodes
         Graphics graph;
-        int row = 0, col = 0;  
+        int row = 0;  
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -164,6 +166,121 @@ namespace flat
             matrix = new int[50, 50];
             vertices = new string[50];
             countnodes = 0;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sd = new SaveFileDialog();
+            sd.Filter = "Documents (*.txt)|*.txt";
+
+            if (sd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sd.FileName, true);
+                String nodes = "";
+                for (int i = 0; i < countnodes; i++)
+                {
+                    nodes = i + "-" + vertices[i];
+                    for (int j = 0; j < countnodes; j++)
+                    {
+                        if (matrix[i, j] != 0)
+                        {
+                            nodes = nodes + "\t" + j + "-" + vertices[j];
+                        }
+                    }
+                    sw.WriteLine(nodes);
+                }
+                MessageBox.Show("File Saved!");
+                sw.Close();
+            }
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog od = new OpenFileDialog();
+            od.Filter = "Text Documents (*.txt)|*.txt";
+            od.ValidateNames = true;
+
+            if (od.ShowDialog() == DialogResult.OK) 
+            {
+                setclear();
+                txtEdge1.Clear();
+                txtEdge2.Clear();
+                txtSource.Clear();
+                txtDestination.Clear();
+                lblDisabled.Text = "";
+                lblEnabled.Text = "";       
+                dataGridView1.Rows.Clear(); 
+                dataGridView1.Columns.Clear();
+                Graphics g = picGraph.CreateGraphics();
+                g.Clear(Color.Silver);
+
+                try
+                {
+                    String line = "";
+                    StreamReader sr = new StreamReader(od.FileName);
+
+                    do
+                    {
+                        line = sr.ReadLine();
+                        String[] adjacent = line.Split('\t');
+                        String[] nodes = adjacent[0].Split('-');
+                        int node1 = Convert.ToInt32(nodes[0]);
+                        int x1 = Convert.ToInt32(nodes[1]);
+                        int y1 = Convert.ToInt32(nodes[2]);
+                        vertices[node1] = x1 + "-" + y1;
+
+
+                        Rectangle rect = new Rectangle(x1, y1, 25, 25);
+                        g.FillEllipse(Brushes.Black, rect);
+                        g.DrawString(node1.ToString(), new Font("Arial", 7), Brushes.White, x1 + 8, y1 + 8);
+
+                        for (int i = 1; i < adjacent.Length; i++)
+                        {
+                            nodes = adjacent[i].Split('-');
+                            int node2 = Convert.ToInt32(nodes[0]);
+                            int x2 = Convert.ToInt32(nodes[1]);
+                            int y2 = Convert.ToInt32(nodes[2]);
+                            vertices[node2] = x2 + "-" + y2;
+
+                            
+                            //draw line
+                            rect = new Rectangle(x2, y2, 25, 25);
+                            g.FillEllipse(Brushes.Black, rect);
+                            g.DrawString(node2.ToString(), new Font("Arial", 7), Brushes.White, x2 + 8, y2 + 8);
+                            g.DrawLine(new Pen(Brushes.Fuchsia, 2), (float)(x1 + 12), (float)(y1 + 12),
+                                     (float)(x2 + 12), (float)(y2 + 12));
+
+                            //calculate distance
+                            double x = (double)(x2 - x1);
+                            double y = (double)(y2 - y1);
+                            double d = Math.Sqrt(Math.Pow(x, 2) + Math.Pow(y, 2));
+
+                            int a = (int)(x1 + x2) / 2;
+                            int b = ((int)(y1 + y2) / 2) - 5;
+
+                            g.DrawString(Math.Round(d, 2).ToString(), new Font("Arial", 9), Brushes.Maroon, a, b);
+
+                            matrix[node1, node2] = Convert.ToInt32(d);
+                            matrix[node2, node1] = Convert.ToInt32(d);
+                        }
+
+                        countnodes++;
+                        dataGridView1.Columns.Add("", (countnodes - 1).ToString());
+                        dataGridView1.AutoResizeColumns();
+                        dataGridView1.Rows.Add();
+                        dataGridView1.Rows[countnodes - 1].HeaderCell.Value = (countnodes - 1).ToString();
+                        dataGridView1.AutoResizeRows();
+
+                        
+
+                    }
+                    while (sr.Peek() != 1);
+                }
+                catch
+                {
+                    MessageBox.Show("Invalid File!");
+                }
+            }
         }
 
         //BFS psuedocode
